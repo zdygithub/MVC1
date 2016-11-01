d2fd584702e3ebd6e475eadbc9ecd4b879ba0624
zdys <ss@ss>
2016-11-01 16:34:34 +0800

    添加BlogController控制器，创建数据库，添加播客列表页面

---
+++ WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var db = new BlogDatabase();
+            db.Database.CreateIfNotExists();//创建数据库
+
+            var lst = db.BlogArticles.OrderByDescending(o => o.Id).ToList();//按指定表达式对集合倒序排序
+            ViewBag.BlogArticles = lst;
+
+            return View();
+        }
+    }
+}
