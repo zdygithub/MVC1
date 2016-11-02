3578ae96c5a40a5f70b2a84d26ebd18d7ec41e9d
stu <stu@qq.com>
2016-11-02 12:20:17 +0800

    博文搜索功能

--- WebApplication1/Controllers/BlogController.cs
+++ WebApplication1/Controllers/BlogController.cs
@@ -9,13 +9,20 @@ namespace WebApplication1.Controllers
     public class BlogController : Controller
     {
         // GET: Blog
-        public ActionResult Index()
+        public ActionResult Index(string q)
         {
             var db = new BlogDatabase();
             db.Database.CreateIfNotExists();//创建数据库
 
-            var lst = db.BlogArticles.OrderByDescending(o => o.Id).ToList();//按指定表达式对集合倒序排序
-            ViewBag.BlogArticles = lst;
+            var lst = db.BlogArticles.AsQueryable();
+
+            if(!string.IsNullOrWhiteSpace(q))
+            {
+                lst = lst.Where(o => o.Subject.Contains(q));
+            }
+
+            ViewBag.BlogArticles = lst.OrderByDescending(o=>o.Subject).ToList();
+            ViewBag.q = q;
 
             return View();
         }
